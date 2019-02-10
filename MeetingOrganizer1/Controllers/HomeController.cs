008dd3b677c5f14d23b62e003f0129a415c56377
iremdaldal <37710613+iremdaldal@users.noreply.github.com>
2019-02-10 14:11:32 +0300

    Add project files.

---
+++ MeetingOrganizer1/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MeetingOrganizer1.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult GetMeeting()
+        {
+            using (DatabaseOrganizerEntities dc = new DatabaseOrganizerEntities())
+            {
+                var meeting = dc.Meetings.ToList();
+                return new JsonResult { Data = meeting, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+            }
+        }
+
+        [HttpPost]
+        public JsonResult SaveMeeting(Meeting e)
+        {
+            var status = false;
+            using (DatabaseOrganizerEntities dc = new DatabaseOrganizerEntities())
+            {
+                dc.Configuration.ProxyCreationEnabled = false;
+                Console.WriteLine(e.MeetingID);
+                if (e.MeetingID > 0)
+                {
+                    //Update the event
+                    var v = dc.Meetings.Where(a => a.MeetingID == e.MeetingID).FirstOrDefault();
+                    if (v != null)
+                    {
+                        v.MeetingSubject = e.MeetingSubject;
+                        v.Start = e.Start;
+                        v.End = e.End;
+                        v.Participants = e.Participants;
+                        v.IsFullDay = e.IsFullDay;
+                        v.ThemeColor = e.ThemeColor;
+                    }
+                }
+                else
+                {
+                    dc.Meetings.Add(e);
+                }
+                dc.SaveChanges();
+                status = true;
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+
+        [HttpPost]
+        public JsonResult DeleteMeeting(int meetingID)
+        {
+            var status = false;
+            using (DatabaseOrganizerEntities dc = new DatabaseOrganizerEntities())
+            {
+                var v = dc.Meetings.Where(a => a.MeetingID == meetingID).FirstOrDefault();
+                if (v != null)
+                {
+                    dc.Meetings.Remove(v);
+                    dc.SaveChanges();
+                    status = true;
+                }
+            }
+            return new JsonResult { Data = new { status = status } };
+        }
+    }
+}
---
+++ MeetingOrganizer1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MeetingOrganizer1.MvcApplication" Language="C#" %>
